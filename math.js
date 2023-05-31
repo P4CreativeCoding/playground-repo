95dcdc0956537ad3514f549eca5c75d11838ddc5
Max Studanski <mstu@posteo.net>
2023-05-31 06:58:36 +0200

    Add divide function

--- math.js
+++ math.js
@@ -12,4 +12,12 @@ function multiply(a, b) {
   return a * b;
 }
 
-module.exports = { add, subtract, multiply };
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+module.exports = { add, subtract, multiply, divide };
